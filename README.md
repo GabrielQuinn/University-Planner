31163e67bf9e69d0bf936e9358aeb81f323245fc
Gabriel Quinn <gabriel@gabrielquinn.com>
2025-03-03 21:33:51 -0500

    Add Structure and index.php

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # University-Planner
+## Created by Gabriel Quinn
+
+### Summary
+Use this website to plan your university schedule / courses. Keep track of the courses you plan on taking, your degree requirements, and current grades. NOT DONE YET!
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>University Planner</title>
+</head>
+<body>
+    <header>
+        <h1>University Planner</h1>
+        <h2>Created by Gabriel Quinn</h2>
+    </header>
+    <section>
+        <h3>Current Term</h3>
+        <ul>
+            <li>
+                <span>COIS-1000</span>
+                <span>Grade: X</span>
+                <button><img src="" alt="State"></button> <!-- CHANGE THIS TO A DROP DOWN LATER -->
+            </li>
+        </ul>
+        <button>Previous Term</button>
+        <button><img src="" alt="Edit Term"></button>
+        <button>Next Term</button>
+    </section>
+    <section>
+        <h3>Degree Requirements</h3>
+        <ul>
+            <li>
+                <span>COIS-1000</span>
+                <span>2.0 Credits</span>
+                <button><img src="" alt="Mark Completed"></button>
+            </li>
+            <li>
+                <span>COIS-1000</span>
+                <span>2.0 Credits</span>
+                <button><img src="" alt="Mark Completed"></button>
+            </li>
+        </ul>
+    </section>
+</body>
+</html>
